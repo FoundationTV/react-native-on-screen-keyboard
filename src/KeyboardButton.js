636ad35cba5a3a040c5599c5df5d58e927a2a208
khateeb_jtv <khateeb@junctiontv.com>
2019-01-21 18:19:42 +0530

    Added source files

---
+++ src/KeyboardButton.js
@@ -0,0 +1,138 @@
+// 'use strict';
+import { Component } from "react";
+import PropTypes from "prop-types";
+import * as React from "react";
+import {
+  Alert,
+  Image,
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight
+} from "react-native";
+
+class KeyboardButton extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { isFocused: false };
+    this.onBlur = this.onBlur.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onPress = this.onPress.bind(this);
+    this.images = {
+      backspace: require("./backspace.png"),
+      shift: require("./shift.png"),
+      spacebar: require("./spacebar.png")
+    };
+  }
+
+  onBlur() {
+    this.setState({ isFocused: false });
+  }
+
+  onFocus() {
+    this.setState({ isFocused: true });
+  }
+
+  onPress() {
+    requestAnimationFrame(() => {
+      this.props.onPress();
+    });
+  }
+
+  render() {
+    const {
+      keyboardButtonContainerStyle,
+      keyboardButtonTextStyle,
+      keyboardButtonTextPressStyle,
+      keyboardButtonStyle,
+      keyboardButtonPressStyle
+    } = this.props;
+    let newStyle = {};
+    if (this.props.width === 2)
+      newStyle = { width: styles.container.width * 2.05 };
+    else if (this.props.width === 3)
+      newStyle = { width: styles.container.width * 3.1 };
+    else if (this.props.width === 4)
+      newStyle = { width: styles.container.width * 4.15 };
+    return (
+      <View style={[styles.container, keyboardButtonContainerStyle, newStyle]}>
+        <TouchableHighlight
+          style={[
+            this.state.isFocused ? styles.buttonPress : styles.button,
+            this.state.isFocused
+              ? keyboardButtonPressStyle
+              : keyboardButtonStyle,
+            newStyle
+          ]}
+          onPress={this.onPress}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+        >
+          {this.props.image ? (
+            <Image
+              style={{ marginLeft: 13 }}
+              resizeMode="center"
+              tintColor={
+                this.state.isFocused
+                  ? keyboardButtonTextPressStyle.color
+                  : keyboardButtonTextStyle.color
+              }
+              source={this.images[this.props.image]}
+            />
+          ) : (
+            <Text
+              style={[
+                this.state.isFocused ? styles.textPress : styles.text,
+                this.state.isFocused
+                  ? keyboardButtonTextPressStyle
+                  : keyboardButtonTextStyle
+              ]}
+            >
+              {this.props.isCaps === true && this.props.width === undefined
+                ? this.props.text.toUpperCase()
+                : this.props.text}
+            </Text>
+          )}
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+KeyboardButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onTapped: PropTypes.func
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 40,
+    height: 40,
+    margin: 1
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "black"
+  },
+  textPress: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "white"
+  },
+  buttonPress: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white"
+  }
+});
+
+export default KeyboardButton;
---
+++ src/index.js
@@ -0,0 +1,171 @@
+// 'use strict';
+import { Component } from "react";
+import PropTypes from "prop-types";
+import * as React from "react";
+import {
+  Alert,
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight
+} from "react-native";
+import KeyboardButton from "./KeyboardButton";
+import keyboardData from "./keyboardData";
+
+class Keyboard extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.allIndices = {
+      emailNormalIndices: [0, 1, 2, 3, 4, 5],
+      emailSpecialIndices: [0, 6, 7, 8, 4, 5],
+      passswordNormalIndices: [0, 1, 2, 3, 9],
+      passswordSpecialIndices: [6, 10, 11, 12, 9],
+      passswordAccentIndices: [0, 13, 14, 15, 9]
+    };
+    let initialIndices = {
+      textEmailAddress: this.allIndices.emailNormalIndices,
+      textPassword: this.allIndices.passswordNormalIndices
+    };
+    this.state = {
+      indices: initialIndices[props.inputType],
+      isCaps: false,
+      isSpecial: false,
+      isAccent: false
+    };
+  }
+
+  onPress(char) {
+    const { inputType, onInput, textInput } = this.props;
+    const { isCaps } = this.state;
+    const {
+      emailNormalIndices,
+      emailSpecialIndices,
+      passswordNormalIndices,
+      passswordSpecialIndices,
+      passswordAccentIndices
+    } = this.allIndices;
+    switch (char) {
+      case "backspace":
+        onInput(textInput.current.props.value.slice(0, -1));
+        break;
+      case "shift":
+        this.setState({ isCaps: !isCaps });
+        break;
+      case "!#$":
+        this.setState({
+          isSpecial: true,
+          isAccent: false,
+          indices:
+            inputType === "textPassword"
+              ? passswordSpecialIndices
+              : emailSpecialIndices
+        });
+        break;
+      case "abc":
+        this.setState({
+          isSpecial: false,
+          isAccent: false,
+          indices:
+            inputType === "textPassword"
+              ? passswordNormalIndices
+              : emailNormalIndices
+        });
+        break;
+      case "àáâ":
+        this.setState({
+          isSpecial: false,
+          isAccent: true,
+          indices: this.allIndices.passswordAccentIndices
+        });
+        break;
+      default:
+        const addition = isCaps ? char.toUpperCase() : char;
+        onInput(textInput.current.props.value + addition);
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { emailNormalIndices, passswordNormalIndices } = this.allIndices;
+    if (this.props.inputType !== nextProps.inputType)
+      this.setState({
+        isSpecial: false,
+        isAccent: false,
+        indices:
+          nextProps.inputType === "textPassword"
+            ? passswordNormalIndices
+            : emailNormalIndices
+      });
+    return true;
+  }
+
+  render() {
+    const {
+      keyboardContainerStyle,
+      keyboardTitleStyle,
+      title,
+      keyboardButtonContainerStyle,
+      keyboardButtonTextStyle,
+      keyboardButtonTextPressStyle,
+      keyboardButtonStyle,
+      keyboardButtonPressStyle
+    } = this.props;
+    const { indices, isSpecial, isAccent, isCaps } = this.state;
+    return (
+      <View style={[styles.container, keyboardContainerStyle]}>
+        <Text style={[styles.title, keyboardTitleStyle]}>{title}</Text>
+        {indices.map(index => (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+            key={index}
+          >
+            {keyboardData[index].map(char => {
+              let text = char.text;
+              if (!isSpecial && !isAccent && text === "abc") text = "!#$";
+              if (isSpecial && !isAccent && text === "!#$") text = "abc";
+              if (!isAccent && !isSpecial && text === "abc") text = "àáâ";
+              if (isAccent && !isSpecial && text === "àáâ") text = "abc";
+              return (
+                <KeyboardButton
+                  image={char.image}
+                  width={char.width}
+                  text={text}
+                  isCaps={isCaps}
+                  onPress={() => this.onPress(text)}
+                  key={text}
+                  keyboardButtonContainerStyle={keyboardButtonContainerStyle}
+                  keyboardButtonTextStyle={keyboardButtonTextStyle}
+                  keyboardButtonTextPressStyle={keyboardButtonTextPressStyle}
+                  keyboardButtonStyle={keyboardButtonStyle}
+                  keyboardButtonPressStyle={keyboardButtonPressStyle}
+                />
+              );
+            })}
+          </View>
+        ))}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingTop: 5,
+    borderWidth: 2,
+    borderRadius: 1,
+    borderColor: "white",
+    backgroundColor: "#69696980"
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "left",
+    color: "#ffffff"
+  }
+});
+
+export default Keyboard;
